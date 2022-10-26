6cd3a422e4f7d083c480a452ef1be5436cf2db35
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-10-26 15:50:07 +0300

    filling js

--- js/app_2.js
+++ js/app_2.js
@@ -2,3 +2,28 @@
 В переменной znak может быть: +, -, *, /, %, ^ (степень).
 Вывести результат математического действия, указанного в переменной znak.
  Оба числа и знак получаются от пользователя.*/
+
+ let x = parseFloat(prompt("Введіть перше число"));
+ let znak = prompt("Введіть знак");
+ let y = parseFloat(prompt("Введіть друге число"));
+ function doMath(x, znak, y){
+    switch(znak){
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            if ( y === 0){
+                return 'Error';
+            }
+            return x / y;
+        case '%':
+            return x % y;
+        case '^':
+            return Math.pow(x, y);
+    }
+}
+
+console.log(doMath(x, znak, y));
